b6be6a6e45ff5377b5643b48b0c3ab4c1aef6755
abdelrahman adel <abdelrahman20191700364@cis.asu.edu.eg>
2024-11-26 20:16:04 +0200

    Add project files.

---
+++ task18/Controllers/DoctorsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task18.Data;
+using task18.Models;
+
+namespace task18.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private ApplicationDBContext _context = new ApplicationDBContext();
+
+        public IActionResult Index()
+        {
+            var doctors = _context.Doctors.ToList();
+            return View(doctors);
+        }
+        
+        public IActionResult CompleteAppointment(int doctorId)
+        {
+            var doctor = _context.Doctors.Find(doctorId);
+            if (doctor!=null) return View(doctor);
+            return RedirectToAction("NotFound");
+        }
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ task18/Data/ApplicationDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using task18.Models;
+
+namespace task18.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-D7HA6NV\\SQL2022;Initial Catalog=Hospital;Integrated Security=True;TrustServerCertificate=True");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Doctor>().HasData(
+                new List<Doctor>
+            {
+            new Doctor { Id = 1, Name = "Dr. John Smith", Specialization = "Cardiology", Img = "doctor1.jpg" },
+            new Doctor { Id = 2, Name = "Dr. Sarah Johnson", Specialization = "Pediatrics", Img = "doctor2.jpg" },
+            new Doctor { Id = 3, Name = "Dr. Emily Davis", Specialization = "Dermatology", Img = "doctor4.jpg" },
+             new Doctor { Id = 4, Name = "Dr. Michael Lee", Specialization = "Orthopedics", Img = "doctor3.jpg" },
+            new Doctor { Id = 5, Name = "Dr. William Clark", Specialization = "Neurology", Img = "doctor5.jpg" },
+            }
+                );
+        }
+
+    }
+}
